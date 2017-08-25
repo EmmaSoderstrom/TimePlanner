6a970ae97caef67fa2a9d976e5b43cce167f9d8e
EmmaSoderstrom <emmaasoderstrom@gmail.com>
2017-08-25 10:02:48 +0200

    6.1 - recyclerview

---
+++ app/src/main/java/se/sockertoppar/timeplanner/RecyclerListFragment.java
@@ -0,0 +1,48 @@
+package se.sockertoppar.timeplanner;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import se.sockertoppar.timeplanner.helper.SimpleItemTouchHelperCallback;
+
+/**
+ * Created by User on 2017-08-25.
+ */
+
+public class RecyclerListFragment extends Fragment {
+
+    private ItemTouchHelper mItemTouchHelper;
+
+    public RecyclerListFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        RecyclerListAdapter adapter = new RecyclerListAdapter();
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(adapter);
+        mItemTouchHelper = new ItemTouchHelper(callback);
+        mItemTouchHelper.attachToRecyclerView(recyclerView);
+    }
+
+}
